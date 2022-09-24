27f2584105677f9b7ea73b682499993abd954bf3
ghintema <harm.intemann@gmail.com>
2022-09-24 18:25:14 -0400

    next commit

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 // Benötigte Funktionen:
 
-const updateCart() {
-    Wieviel waren sind schon im Korb?
-    Wieviel waren sollen hinzugefügt werden?
+// Changing hamburger-icon to cross
+const myFunction = (event) => {
+    event.classList.toggle("change");
+  }
+
+
+const updateCart = () => {
+    // Wieviel waren sind schon im Korb?
+    // Wieviel waren sollen hinzugefügt werden?
 }
 
-const change(n) {
-    let current = document.getElementById('number-of-items').innerHTML
-    current = parseInt(current);
-    if (current > 0 ) {
-        current += n;
-        document.getElementById('number-of-items').innerHTML = ${current};
-    }
-}
+
+// const change = (n) => {
+//     let current = document.getElementById('number-of-items').innerHTML
+//     current = parseInt(current);
+//     if (current > 0 ) {
+//         current += n;
+//         document.getElementById('number-of-items').innerHTML = ${current};
+//     }
+// }
